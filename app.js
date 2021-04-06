3e9a879f4fd0b1f8f1aba0dec51be26ed483d62b
nmparfait <mbamu.developer@gmail.com>
2021-04-06 10:46:01 -0400

    add the element and set back to default

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const grocery = document.getElementById('grocery');
 const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
+const submitBtn = document.querySelector('.submit-btn');
 // const editBtn = document.querySelector('.edit-btn');
 // const deleteBtn = document.querySelector('.delete-btn');
 // edit option
@@ -22,7 +23,32 @@ function addItem(e) {
   const value = grocery.value;
   const id = new Date().getTime().toString();
   if (value && !editFlag) {
-    console.log('add item to the list');
+    const element = document.createElement('article');
+    //add class
+    element.classList.add('grocery-item');
+    //add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    // append to the list
+    list.appendChild(element);
+    //display alert
+    displayAlert('item added to the list', 'success');
+    //show my container
+    container.classList.add('show-container');
+    // add to local Storage
+    addToLocalStorage(id, value);
+    //set back to default
+    setBackToDefault();
   } else if (value && editFlag) {
     console.log('we are editing');
   } else {
@@ -43,6 +69,17 @@ function displayAlert(text, action) {
   }, 1000);
 }
 
-// ****** LOCAL STORAGE **********
+// set back to default
+function setBackToDefault() {
+  console.log('set back to default');
+  grocery.value = '';
+  editFlag = false;
+  editID = '';
+  submitBtn.textContent = 'submit';
+}
 
+// ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+  console.log('added to local storage');
+}
 // ****** SETUP ITEMS **********

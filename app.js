19f4bf43d1551d005428efd8d8c682fbaf27c1f0
nmparfait <mbamu.developer@gmail.com>
2021-04-06 09:43:41 -0400

    selecting items

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 // ****** SELECT ITEMS **********
-
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+// const editBtn = document.querySelector('.edit-btn');
+// const deleteBtn = document.querySelector('.delete-btn');
 // edit option
+let editElement;
+let editFlag = false;
+let editID = '';
 
 // ****** EVENT LISTENERS **********
 
